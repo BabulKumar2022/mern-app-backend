1b1a385f8e6c510dca36c904ae923347fdeed19d
unknown <bb3583400@gmail.com>
2023-03-23 00:07:20 +0600

    all

--- index.js
+++ index.js
@@ -49,6 +49,15 @@ app.post("/addProduct", async(req, res)=>{
     let product = new Product(req.body);
     let result = await product.save();
     res.send(result);
+});
+// get all products
+app.get("/products", async (req, res) =>{
+    const products = await Product.find();
+    if(products.length > 0){
+        res.send(products);
+    }else{
+        res.send({result: "No product found"})
+    }
 })
 
 
